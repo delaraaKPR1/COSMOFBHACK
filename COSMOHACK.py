fbebeea16d5d7f2416a2ac464708d1246a8255b3
delaraaKPR1 <73485996+delaraaKPR1@users.noreply.github.com>
2020-10-26 22:28:41 +0700

    Add files via upload

---
+++ COSMOHACK.py
@@ -0,0 +1,87 @@
+import tqdm, os
+import requests, time
+ 
+#cieeeeee mau nyari sandi yaaa yaaaa yaaa
+#gpp kok 
+#saya cuma mau nguji logika anak indonesia ajaaaaa
+#supaya tau yg mana yg ahli atau gak serius
+#saya jugaf belajar dari ngerecode
+#maaf kalo kurang rapi hehe
+#oh iya kalo ada orang perawang.. mampir ke kpr 1 lahh yukkk
+def main():
+    os.system("clear")
+    time.sleep(2)
+    print("LOADING...")
+    from tqdm import tqdm
+    for i in tqdm(range(5)):
+         time.sleep(.100)
+    
+
+    print("+----------------------------------------------+")
+    time.sleep(.100)
+    print("|                   COSMOHACK                  |") 
+    time.sleep(.100)
+    print("+----------------------------------------------+")
+    time.sleep(.100)
+    print("| CREATED BY VJY ANAK PERAWANG,RIAU            |")
+    time.sleep(.100)
+    print("|                                              |")
+    time.sleep(.100)
+    print("| ALL RIGHTS REVERSED 2020                     |")
+    time.sleep(.100)
+    print("| PASSWORDNYA tanya aja ke 089527511264        |")
+    time.sleep(.100)
+    print("+----------------------------------------------+")
+    time.sleep(1)
+    xy = input("PASSWORDNYA : ")
+    #ELU TAU LAH KAN.... :)
+    if (xy == "KUY KE KPR 1"):
+        lanjut()
+    if (xy != "KUY KE KPR 1"):
+        print("PASSWORDNYA SALAH BRO...")
+        main()
+    mon()
+def lanjut():
+    time.sleep(3)
+    #pake algoritma NXVV DI JARINGAN TOR AKU.. DAN LETAK DISINI
+    #KALO GAK ADA ALGORITMA NYA GAK AKAN WORK NIH TOOLS
+    
+    #PASANG ALGORITMA DISINI.....
+
+    os.system("clear")
+    time.sleep(2)
+    print("+------------------------------------------------------+")
+    print("+                   memulai proses                     +")
+    print("+------------------------------------------------------+")
+    print("LOGIN..")
+    ihoo= input("ID/email/NO HP : ")
+    sgg = input("password : ")
+    HUHA()
+def HUHA():
+    print("")
+    print("SEDANG LOGIN....")
+    from tqdm import tqdm
+    for i in tqdm(range(100)):
+         time.sleep(.100)
+    time.sleep(2)
+    print("")
+    print("Mengecek akun kamu.. mohon tunggu...")
+    print("")
+    time.sleep(15)
+    print("SAYANGNYA AKUN KAMU TERKENA CHECKPOINT..")
+    print("")
+    time.sleep(4)
+    print("MEMPERBAIKI CHECKPOINT..")
+    from tqdm import tqdm
+    for i in tqdm(range(15)):
+         time.sleep(.100)
+    print("")
+    print("( CHECKPOINT DITUNTASKAN.. )                                                         TETAPI TETAP TIDAK BISA MASUK, KARENA JIKA DIPAKSAKAN... MAKA AKAN TERJADI LOOP SECARA TERUS MENERUS... SAMPE KUOTA KAMU HABIS... SAYA AKAN BERUSAHA MENGHADIRKAN TOOLS GRATIS BAGI SEMUA ORG..... GOOD BYE")
+    print("")
+    print("TOOLS ini akan keluaur otomatis dalam 25 detik")
+    from tqdm import tqdm
+    for i in tqdm(range(25)):
+         time.sleep(1)
+ 
+    exit()
+main()
